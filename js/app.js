292903d9db4ef12d9bcd931ea3cd3c26b7fb8ce6
JeffersonCarlosLima <jeffersoncl200@gmail.com>
2023-09-24 21:21:39 -0300

    correcao de responsividade

--- js/app.js
+++ js/app.js
@@ -30,6 +30,7 @@ cardapio.metodos = {
             let temp = cardapio.templates.item
             .replace(/\${img}/g, e.img)
             .replace(/\${name}/g, e.name)
+            .replace(/\${description}/g, e.dsc)
             .replace(/\${id}/g, e.id)
             .replace(/\${price}/g, e.price.toFixed(2).replace('.',","));
             //botão ver mais clicado 12 itens
@@ -53,55 +54,17 @@ cardapio.templates = {
                 <div class="img-produto">
                     <img src="\${img}"/>
                 </div>
-                <p class="title-produto text-center mt-4">
-                    <b>\${name}</b>
+                <div class="dados-produto">
+                <p class="title-produto mt-4">
+                <b>\${name}</b>
                 </p>
-                <p class="price-produto text-center">
-                    <b>R$ \${price}</b>
+                <p class="description">\${description}</p>
+                <p class="price-produto">
+                <b>R$ \${price}</b>
                 </p>
-                <div class="add-carrinho">
-                    <spam class="btn-menos" onclick="cardapio.metodos.diminuirQuantidadeItens('\${id}')"><i class="fas fa-minus"></i></spam>
-                    <spam class="add-numero-itens" id="qtd-\${id}">0</spam>
-                    <spam class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeItens('\${id}')"><i class="fas fa-plus"></i></spam>
-                    <spam class="btn btn-add" onclick="cardapio.metodos.adicinarCarrinho('\${id}')"><i class="fas fa-shopping-bag"></i></spam>
                 </div>
             </div>
         </div>
     `,
-    intensCarrinho:
-    `
-    <div class="col-12 item-carrinho">
-        <div class="info-produto">
-            <div class="img-produto">
-                <img class="img-produto" src="\${img}" alt="">
-            </div>
-            <div class="dados-produto">
-                <p class="title-produto"><b>\${name}</b></p>
-                <p class="price-produto"><b>R$ \${price}</b></p>
-            </div>
-        </div>
-        <div class="add-carrinho">
-            <spam class="btn-menos" onclick="cardapio.metodos.diminuirQuantidadeCarrinho('\${id}')"><i class="fas fa-minus"></i></spam>
-            <spam class="add-numero-itens" id="qtd-carrinho-\${id}">\${qntd}</spam>
-            <spam class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')"><i class="fas fa-plus"></i></spam>
-            <spam class="btn btn-remove" onclick="cardapio.metodos.removeItemCarrinho('\${id}')"><i class="fas fa-times"></i></spam>
-        </div>
-    </div>
-    `,
-itensResumo:
-    `<div class="col-12  item-carrinho-resumo">
-        <div class="img-produto-resumo">
-            <img src="\${img}" alt="imagem do produto">
-        </div>
-        <div class="dados-produto">
-            <p class="title-produto-resumo"><b>\${name}</b></p>
-            <p class="price-produto-resumo">
-                <b>R$ \${price}</b>
-            </p>
-        </div>
-        <p class="quantidade-produto-resumo">
-            x <b>\${qntd}</b>
-        </p>
-    </div>`
 };
 
